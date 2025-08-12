7aed824c9a05b102ecaa8f8ce72001707b9eea19
Ali Alashawal <aliashwal03@gmail.com>
2025-08-12 23:13:40 +0300

    fixed booking system

--- models/booking.js
+++ models/booking.js
@@ -1,41 +1,37 @@
-const mongoose = require('mongoose')
+const mongoose = require('mongoose');
 
-const bookingSchema = new mongoose.Schema(
-  {
-    ticketNumber: {
-      type: String,
-      required: true,
-      unique: true,
-    },
-    user: {
-      type: mongoose.Schema.Types.ObjectId,
-      ref: 'User',
-      required: true,
-    },
-    movie: {
-      type: mongoose.Schema.Types.ObjectId,
-      ref: 'Movie',
-      required: true,
-    },
-    seats: [
-      {
-        type: mongoose.Schema.Types.ObjectId, 
-        ref: 'Seats',
-        required: true,
-      },
-    ],
-    timing: {
-      type: Date,
-      required: true,
-    },
-    bookedAt: {
-      type: Date,
-      default: Date.now,
-    },
+const bookingSchema = new mongoose.Schema({
+  ticketNumber: {
+    type: String,
+    required: true,
+    unique: true,
   },
-  { timestamps: true }
-)
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+    required: true,
+  },
+  movie: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Movie',
+    required: true,
+  },
+  seats: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      required: true,
+    },
+  ],
+  timing: {
+    type: Date,
+    required: true,
+  },
+  bookedAt: {
+    type: Date,
+    default: Date.now,
+  },
+}, { timestamps: true });
 
-const Booking = mongoose.model('Booking', bookingSchema)
+const Booking = mongoose.model('Booking', bookingSchema);
 
-module.exports = Booking
+module.exports = Booking;
